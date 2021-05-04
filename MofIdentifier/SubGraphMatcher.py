13c549948c6a6500b7b5bfc918ddb4553c7829bc
David Hawley <mdavid4@byu.edu>
2021-05-04 11:20:35 -0600

    Add nodes with elements to networkx graphs

--- MofIdentifier/SubGraphMatcher.py
+++ MofIdentifier/SubGraphMatcher.py
@@ -1,13 +1,23 @@
 import networkx.algorithms.isomorphism as iso
 import networkx as nx
 from MofIdentifier import XyzReader, MofReader
+from MofIdentifier.MofBondCreator import MofBondCreator
+from MofIdentifier.XyzBondCreator import XyzBondCreator
 
 
 def find_ligand_in_mof(ligand, mof):
     lGraph = nx.Graph()
-    lGraph.add_nodes_from(ligand.atoms)
+    for atom in ligand.atoms:
+        lGraph.add_node(atom, element=atom.type_symbol)
+    for atom in ligand.atoms:
+        for bonded_atom in atom.bondedAtoms:
+            lGraph.add_edge(atom, bonded_atom)
     mGraph = nx.Graph()
-    mGraph.add_nodes_from(mof.atoms)
+    for atom in mof.atoms:
+        mGraph.add_node(atom, element=atom.type_symbol)
+    for atom in mof.atoms:
+        for bonded_atom in atom.bondedAtoms:
+            mGraph.add_edge(atom, bonded_atom)
     graph_matcher = iso.GraphMatcher(lGraph, mGraph, node_match=iso.categorical_node_match(['element'], [None]))
     mof_contains_ligand = graph_matcher.subgraph_is_isomorphic()
     return mof_contains_ligand
@@ -15,6 +25,10 @@ def find_ligand_in_mof(ligand, mof):
 
 if __name__ == '__main__':
     ligand = XyzReader.read_xyz('Benzene.xyz')
+    bond_creator = XyzBondCreator()
+    bond_creator.connect_atoms(ligand)
     mof = MofReader.read_mof('smod7-pos-1.cif')
+    bond_creator = MofBondCreator(mof)
+    bond_creator.connect_atoms()
     contains_ligand = find_ligand_in_mof(ligand, mof)
     print(contains_ligand)
