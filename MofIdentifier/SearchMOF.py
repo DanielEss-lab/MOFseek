cd8e659d1e65d39c5ec5cca2668dc9061ffacd59
spark58 <shersy716@gmail.com>
2021-05-13 20:52:53 -0600

    2021 / 05 / 13 - edit SearchMOF.py class. Need to think about exception that user types.

--- MofIdentifier/SearchMOF.py
+++ MofIdentifier/SearchMOF.py
@@ -6,45 +6,51 @@ from pathlib import Path
 from XyzBondCreator import XyzBondCreator
 
 
-def ligands_list():
-    bond_creator = XyzBondCreator()
+def get_ligand_list():
+    # An empty list for ligands that user wants to search
+    ligands = []
 
-    Benzene = XyzReader.read_xyz(Path(__file__).parent / "ligandsWildcards/BenzeneWildCard.xyz")
-    bond_creator.connect_atoms(Benzene)
+    # User specifies xyz file name - CO2_1.xyz
+    user_input = input("Enter a xyz file name: (Example: CO2_1.xyz). If you want to quit, type \"quit\"\n")
 
-    CO2_1 = XyzReader.read_xyz(Path(__file__).parent / 'ligandsWildcards/CO2_1.xyz')
-    bond_creator.connect_atoms(CO2_1)
+    if user_input == "quit":
+        exit()
 
-    H2O_1 = XyzReader.read_xyz(Path(__file__).parent / 'ligandsWildcards/H2O_1.xyz')
-    bond_creator.connect_atoms(H2O_1)
+    # Use while loop
+    while user_input != "quit":
+        ligands.append(user_input)
+        print("Enter a xyz file name: (Example: CO2_1.xyz). If you want to quit, type \"quit\"")
+        user_input = input()
 
-    H2O_2 = XyzReader.read_xyz(Path(__file__).parent / 'ligandsWildcards/H2O_2.xyz')
-    bond_creator.connect_atoms(H2O_2)
-
-    HSO4_1 = XyzReader.read_xyz(Path(__file__).parent / 'ligandsWildcards/HSO4_1.xyz')
-    bond_creator.connect_atoms(HSO4_1)
-
-    HSO4_2 = XyzReader.read_xyz(Path(__file__).parent / 'ligandsWildcards/HSO4_2.xyz')
-    bond_creator.connect_atoms(HSO4_2)
-
-    M6_node = XyzReader.read_xyz(Path(__file__).parent / 'ligandsWildcards/M6_node.xyz')
-    bond_creator.connect_atoms(M6_node)
-
-    SO4_1 = XyzReader.read_xyz(Path(__file__).parent / 'ligandsWildcards/SO4_1.xyz')
-    bond_creator.connect_atoms(SO4_1)
-
-    SO4_2 = XyzReader.read_xyz(Path(__file__).parent / 'ligandsWildcards/SO4_2.xyz')
-    bond_creator.connect_atoms(SO4_2)
-
-    SO4_3 = XyzReader.read_xyz(Path(__file__).parent / 'ligandsWildcards/SO4_3.xyz')
-    bond_creator.connect_atoms(SO4_3)
-
-    ligands = {Benzene, CO2_1, H2O_1, H2O_2, HSO4_1, HSO4_2, M6_node, SO4_1, SO4_2, SO4_3}
+    print("Searching... \n")
 
     return ligands
 
 
+def read_xyzFiles_from_list():
+    files = get_ligand_list()
+
+    ligands = []
+    bond_creator = XyzBondCreator()
+
+    for file in os.listdir(Path(__file__).parent / "ligandsWildcards"):
+        if file.endswith(".xyz"):
+            for ligand in files:
+                if file == ligand:
+                    ligand_xyz = XyzReader.read_xyz(str(Path(__file__).parent / "ligandsWildcards") + "/" + ligand)
+                    bond_creator.connect_atoms(ligand_xyz)
+                    ligands.append(ligand_xyz)
+
+    return ligands, files
+
+
 def read_Cif(file_path):
+    # list of mofs that has specific ligands
+    mofs = []
+
+    # Get ligands from user
+    ligands_files, ligands_name = read_xyzFiles_from_list()
+
     # Change the directory
     os.chdir(file_path)
 
@@ -52,23 +58,28 @@ def read_Cif(file_path):
         # Check whether file is in text format or not
         if file.endswith(".cif"):
             mof = CifReader.read_mof(file)
-            print(mof)
-            print("Elements in mof:", *mof.elementsPresent)
-            search_ligands(mof)
-            print("Ligand in mof:", *mof.ligandsPresent, "\n")
+            # print(mof)
+            # print("Elements in mof:", *mof.elementsPresent)
+            search_ligands(mofs, mof, ligands_files)
+            # print("Ligand in mof:", *mof.ligandsPresent, "\n")
+
+    print(*ligands_name, "is(are) in the following file(s):")
+    print(*mofs, sep=", ")
 
 
-def search_ligands(mof):
+def search_ligands(mofs, mof, ligands_list):
     bond_creator = XyzBondCreator()
     bond_creator.connect_atoms(mof)
-    # if find_ligand_in_mof returns true, then put the ligand into mof.ligandsPresent set
-    for ligand in ligands_list():
-        if SubGraphMatcher.find_ligand_in_mof(ligand, mof):
-            mof.ligandsPresent.add(Path(ligand.label).stem)
+    # if find_ligand_in_mof returns true, then put the mofs into the list
+    for ligand in ligands_list:
+        if not SubGraphMatcher.find_ligand_in_mof(ligand, mof):
+            return
+    # Add to set of mofs
+    mofs.append(mof.label)
 
 
 if __name__ == '__main__':
     # Folder Path
     user_path = input("Enter a folder path that has .cif files: (Example: \\Users\shers\Desktop\Chem\structure_10143)\n")
-    # path = "\'" + user_path
     read_Cif(user_path)
+    # read_xyzFiles_from_list()
