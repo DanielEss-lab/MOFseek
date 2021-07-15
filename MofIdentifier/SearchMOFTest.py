c7d9ba65ab0bcec1d801c39a934779aefc1716f1
David Hawley <mdavid4@byu.edu>
2021-07-15 12:19:35 -0600

    Fix SearchMOFTest to use the correct name for H20_bonded

--- MofIdentifier/SearchMOFTest.py
+++ MofIdentifier/SearchMOFTest.py
@@ -10,7 +10,7 @@ from MofIdentifier.fileIO import SmilesReader, CifReader
 class SearchMofTest(unittest.TestCase):
     def test_M6_node_mofs(self):
         user_path = str(Path(__file__).parent / "mofsForTests")
-        ligand_file_names = ["M6_node.xyz", "H2O_1.xyz"]
+        ligand_file_names = ["M6_node.xyz", "H2O_bonded.xyz"]
         ligands = SearchMOF.read_ligands_from_files(ligand_file_names)
         mofs = CifReader.get_all_mofs_in_directory(user_path)
         good_mofs = [mof for mof in mofs if SubGraphMatcher.mof_has_all_ligands(mof, ligands)]
