54509e811ca2dc698893600a26370dab65334523
David Hawley <mdavid4@byu.edu>
2021-08-04 09:32:03 -0600

    Create functions for finding oxygens/hydrogens to add/reomve in each phase of insertion or deletion

--- MetalModifier/main.py
+++ MetalModifier/main.py
@@ -81,6 +81,199 @@ def get_metal_of_node(node):
             type_s = atom.type_symbol
     return type_s
 
+# Hydrogen locations come in 6 groups on M6 nodes:
+# 0) The first hydrogens on the oxygens connected to the metals. These would almost never be removed.
+# 1) The first 4 mew-3-oxyls
+# 2) The second hydrogens on the first oxygen connected to each metal
+# 3) The second hydrogens on the second oxygen connected to each metal. Ie you shouldn't have one metal with 2 H2O
+#           and another one with 2 OH; rather, both should have 1 H20 and 1 OH
+# 4) Each sulfate group can have one of its oxygens connect to a H
+# 5) The other 4 mew-3-oxyls
+# When adding, add from the top down. When deleting, delete from the bottom up
+
+
+def get_relevant_group_0_atoms(cluster):
+    # 0) The first hydrogens on the oxygens connected to the metals. These would almost never be removed.
+    delete_eligible_hydrogens = []
+    add_eligible_oxygens = []
+    for aux in cluster[1]:
+        if any(atom.type_symbol == 'S' or atom.type_symbol == 'P' or atom.type_symbol == 'C' for atom in aux.atoms):
+            continue
+        oxygen = None
+        for atom in aux.atoms:
+            if atom.type_symbol == 'O' and oxygen is None:
+                oxygen = atom
+            elif atom.type_symbol == 'O':
+                continue  # Two oxygens, therefore doesn't fit requirements
+        hydrogen = None
+        for atom in oxygen.bondedAtoms:
+            if atom.type_symbol == 'H' and hydrogen is None:
+                hydrogen = atom
+            elif atom.type_symbol == 'H':
+                assert(len([neighbor for neighbor in oxygen.bondedAtoms if neighbor.type_symbol == 'H']) < 2)
+        if hydrogen is None:
+            add_eligible_oxygens.append(oxygen)
+        else:
+            delete_eligible_hydrogens.append(hydrogen)
+    delete_eligible_hydrogens.sort()
+    add_eligible_oxygens.sort()
+    return delete_eligible_hydrogens, add_eligible_oxygens
+
+
+def get_relevant_group_1_atoms(cluster, mof):
+    # 1) The first 4 mew-3-oxyls
+    delete_eligible_hydrogens = []
+    add_eligible_oxygens = []
+    for atom in cluster[0].atoms:
+        if atom.type_symbol != 'O' or len([neighbor for neighbor in atom.bondedAtoms if neighbor.is_metal()]) != 3:
+            continue
+        hydrogen = None
+        for neighbor in atom.bondedAtoms:
+            if neighbor.type_symbol == 'H' and hydrogen is None:
+                hydrogen = neighbor
+            elif neighbor.type_symbol == 'H':
+                assert(len([neighbor for neighbor in atom.bondedAtoms if neighbor.type_symbol == 'H']) < 2)
+        if hydrogen is None:
+            add_eligible_oxygens.append(atom)
+        else:
+            delete_eligible_hydrogens.append(hydrogen)
+    assert(len(delete_eligible_hydrogens) == 4 or len(delete_eligible_hydrogens) == 0)
+    assert(len(add_eligible_oxygens) == 8 or len(add_eligible_oxygens) == 4)
+    if add_eligible_oxygens == 8:
+        add_eligible_oxygens.sort()
+        first_pole = add_eligible_oxygens[0]
+        others = [(pole, Distances.distance_across_unit_cells(
+            first_pole, pole, mof.angles, mof.lengths)) for pole in add_eligible_oxygens[1:]]
+        others.sort(key=lambda pair: pair[1])
+        add_eligible_oxygens = [first_pole].extend(others[3:6])  # Not the three closest, neither the one farthest
+    delete_eligible_hydrogens.sort()
+    add_eligible_oxygens.sort()
+    return delete_eligible_hydrogens, add_eligible_oxygens
+
+
+def get_relevant_groups_2_and_3_atoms(cluster):
+    # 2) The second hydrogen atoms on the first oxygen connected to each metal
+    # 3) The second hydrogens on the second oxygen connected to each metal. Ie you shouldn't have one metal with 2 H2O
+    # and another one with 2 OH; rather, both should have 1 H20 and 1 OH
+    h_by_metal = dict()
+    o_by_metal = dict()
+    for aux in cluster[1]:
+        if any(atom.type_symbol == 'S' or atom.type_symbol == 'P' or atom.type_symbol == 'C' for atom in aux.atoms):
+            continue
+        oxygen = None
+        for atom in aux.atoms:
+            if atom.type_symbol == 'O' and oxygen is None:
+                oxygen = atom
+            elif atom.type_symbol == 'O':
+                continue  # Two oxygens, therefore doesn't fit requirements
+        metal = None
+        for atom in oxygen.bondedAtoms:
+            if atom.is_metal() and metal is None:
+                metal = atom
+            elif atom.type_symbol == 'O':
+                raise Exception('Oxygen connected to two metal atoms should not be part of an aux group. '
+                                'Please contact developer.')
+        if metal in o_by_metal:
+            o_by_metal[metal].append(oxygen)
+        else:
+            o_by_metal[metal] = [oxygen]
+        hydrogen = []
+        for atom in oxygen.bondedAtoms:
+            if atom.type_symbol == 'H':
+                hydrogen.append(atom)
+        if metal in h_by_metal:
+            h_by_metal[metal].extend(hydrogen)
+        else:
+            h_by_metal[metal] = [hydrogen]
+    group_2_delete_hydrogens = []
+    group_2_add_oxygens = []
+    group_3_delete_hydrogens = []
+    group_3_add_oxygens = []
+    for metal in h_by_metal:
+        hydrogens = h_by_metal[metal]
+        if len(hydrogens) == 2:
+            group_2_add_oxygens.append(o_by_metal[metal][0])
+            group_3_add_oxygens.append(o_by_metal[metal][1])
+        elif len(hydrogens) == 3:
+            if len([neighbor for neighbor in o_by_metal[metal][0].bondedAtoms if neighbor.type_symbol == 'H']) == 2:
+                occupied_oxygen = o_by_metal[metal][0]
+                other_oxygen = o_by_metal[metal][1]
+            else:
+                occupied_oxygen = o_by_metal[metal][1]
+                other_oxygen = o_by_metal[metal][0]
+            group_3_add_oxygens.append(other_oxygen)
+            group_2_delete_hydrogens.append([neighbor for neighbor in occupied_oxygen.bondedAtoms if neighbor.type_symbol == 'H'][0])
+        elif len(hydrogens) == 4:
+            group_3_delete_hydrogens.append([neighbor for neighbor in o_by_metal[metal][0].bondedAtoms if neighbor.type_symbol == 'H'][0])
+            group_2_delete_hydrogens.append([neighbor for neighbor in o_by_metal[metal][1].bondedAtoms if neighbor.type_symbol == 'H'][0])
+        else:
+            raise Exception(f'Error: metal has {len(hydrogens)} protons attached during the group 2/3 phase')
+    group_2_delete_hydrogens.sort()
+    group_2_add_oxygens.sort()
+    group_3_delete_hydrogens.sort()
+    group_3_add_oxygens.sort()
+    return group_3_delete_hydrogens.extend(group_2_delete_hydrogens), group_2_add_oxygens.extend(group_3_add_oxygens)
+
+
+def get_relevant_group_4_atoms(cluster):
+    # 4) Each sulfate group can have one of its oxygens connect to a H
+    delete_eligible_hydrogens = []
+    add_eligible_oxygens = []
+    for aux in cluster[1]:
+        sulfur = None
+        for atom in aux:
+            if atom.type_symbol == 'S':
+                sulfur = atom
+        if sulfur is None:
+            continue
+        eligible_oxygens = []
+        for atom in sulfur.bondedAtoms:
+            if atom.type_symbol == 'O' and len([neighbor for neighbor in atom.bondedAtoms if neighbor.is_metal()]) == 0:
+                eligible_oxygens.append(atom)
+        hydrogen = None
+        for oxygen in eligible_oxygens:
+            for neighbor in oxygen.bondedAtoms:
+                if neighbor.type_symbol == 'H':
+                    hydrogen = neighbor
+        if hydrogen is None:
+            add_eligible_oxygens.append(eligible_oxygens[0])
+        else:
+            delete_eligible_hydrogens.append(hydrogen)
+    delete_eligible_hydrogens.sort()
+    add_eligible_oxygens.sort()
+    return delete_eligible_hydrogens, add_eligible_oxygens
+
+
+def get_relevant_group_5_atoms(cluster, mof):
+    # 1) The other 4 mew-3-oxyls
+    delete_eligible_hydrogens = []
+    add_eligible_oxygens = []
+    for atom in cluster[0].atoms:
+        if atom.type_symbol != 'O' or len([neighbor for neighbor in atom.bondedAtoms if neighbor.is_metal()]) != 3:
+            continue
+        hydrogen = None
+        for neighbor in atom.bondedAtoms:
+            if neighbor.type_symbol == 'H' and hydrogen is None:
+                hydrogen = neighbor
+            elif neighbor.type_symbol == 'H':
+                assert(len([neighbor for neighbor in atom.bondedAtoms if neighbor.type_symbol == 'H']) < 2)
+        if hydrogen is None:
+            add_eligible_oxygens.append(atom)
+        else:
+            delete_eligible_hydrogens.append(hydrogen)
+    assert(len(delete_eligible_hydrogens) == 8 or len(delete_eligible_hydrogens) == 4)
+    assert(len(add_eligible_oxygens) == 4 or len(add_eligible_oxygens) == 0)
+    if delete_eligible_hydrogens == 8:
+        delete_eligible_hydrogens.sort()
+        first_pole = delete_eligible_hydrogens[7]
+        others = [(pole, Distances.distance_across_unit_cells(
+            first_pole, pole, mof.angles, mof.lengths)) for pole in delete_eligible_hydrogens[0:7]]
+        others.sort(key=lambda pair: pair[1])
+        delete_eligible_hydrogens = [first_pole].extend(others[3:6])  # Not the three closest, neither the one farthest
+    delete_eligible_hydrogens.sort()
+    add_eligible_oxygens.sort()
+    return delete_eligible_hydrogens, add_eligible_oxygens
+
 
 def get_atoms_to_delete(cluster, num_to_delete):
     priority_atoms = []
@@ -112,7 +305,7 @@ def get_atoms_to_delete(cluster, num_to_delete):
                             num_hydrogens_marked_for_deletion += 1
                         else:
                             raise Exception('The developer did not account for aux groups with 3 or more Hydrogens. '
-                                            'Please ask him to update the progrm if you need to use it with such.')
+                                            'Please ask him to update the program if you need to use it with such.')
         second_hydrogens.sort()
         eligible_atoms.sort()
         priority_atoms.extend(second_hydrogens)
