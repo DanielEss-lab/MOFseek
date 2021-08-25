9835f95f6d1a3cbf898205e170b208abc2f719de
David Seek Hawley <shadowyfour@gmail.com>
2021-08-25 10:20:10 -0600

    Make MoleculeView raise an exception when non-db type MOFs Ligands or SBUs are given

--- GUI/Views/MoleculeView.py
+++ GUI/Views/MoleculeView.py
@@ -1,14 +1,13 @@
 from GUI.Views import LigandView, SBUView, MOFView
 from DAO import LigandDatabase, SBUDatabase, MOFDatabase
-from MofIdentifier.Molecules import Ligand, SBU, MOF
 
 
 def make_view(parent, molecule):
-    if isinstance(molecule, LigandDatabase.LigandDatabase) or isinstance(molecule, Ligand.Ligand):
+    if isinstance(molecule, LigandDatabase.LigandDatabase):
         return LigandView.View(parent, molecule)
-    elif isinstance(molecule, SBUDatabase.SBUDatabase) or isinstance(molecule, SBU.SBU):
+    elif isinstance(molecule, SBUDatabase.SBUDatabase):
         return SBUView.View(parent, molecule)
-    elif isinstance(molecule, MOFDatabase.MOFDatabase) or isinstance(molecule, MOF.MOF):
+    elif isinstance(molecule, MOFDatabase.MOFDatabase):
         return MOFView.View(parent, molecule)
     else:
         raise Exception(f"MoleculeView requires a ligand or SBU to construct, not a {type(molecule)}")
