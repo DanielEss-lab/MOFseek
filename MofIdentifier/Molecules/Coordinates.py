50a87bf62f2fbaee95a87f17dac6afd2fc9fb4c2
David Hawley <shadowyfour@gmail.com>
2022-02-17 14:55:00 -0700

    Use math methods instead of np when operating on single values (30% speed increase)

--- MofIdentifier/Molecules/Coordinates.py
+++ MofIdentifier/Molecules/Coordinates.py
@@ -1,34 +1,36 @@
+import math
+
 import numpy as np
 
 
 def conversion_to_Cartesian(atom_a, atom_b, atom_c, angles, lengths, volume):
-    alpha = np.deg2rad(angles[0])
-    beta = np.deg2rad(angles[1])
-    gamma = np.deg2rad(angles[2])
+    alpha = angles[0] * math.pi / 180
+    beta = angles[1] * math.pi / 180
+    gamma = angles[2] * math.pi / 180
     length_a = lengths[0]
     length_b = lengths[1]
     length_c = lengths[2]
 
-    value_of_trig = (np.cos(alpha) - (np.cos(beta) * np.cos(gamma))) / np.sin(gamma)
+    value_of_trig = (math.cos(alpha) - (math.cos(beta) * math.cos(gamma))) / math.sin(gamma)
 
-    matrix = np.array([[length_a, (length_b * np.cos(gamma)), (length_c * np.cos(beta))],
-                       [0, (length_b * np.sin(gamma)), length_c * value_of_trig],
-                       [0, 0, volume / (length_a * length_b * np.sin(gamma))]])
+    matrix = np.array([[length_a, (length_b * math.cos(gamma)), (length_c * math.cos(beta))],
+                       [0, (length_b * math.sin(gamma)), length_c * value_of_trig],
+                       [0, 0, volume / (length_a * length_b * math.sin(gamma))]])
 
     return np.matmul(matrix, np.array([atom_a, atom_b, atom_c]))
 
 
 def convert_to_fractional(atom_x, atom_y, atom_z, mof):
-    alpha = np.deg2rad(mof.angles[0])
-    beta = np.deg2rad(mof.angles[1])
-    gamma = np.deg2rad(mof.angles[2])
+    alpha = mof.angles[0] * math.pi / 180
+    beta = mof.angles[1] * math.pi / 180
+    gamma = mof.angles[2] * math.pi / 180
     a = mof.fractional_lengths[0]
     b = mof.fractional_lengths[1]
     c = mof.fractional_lengths[2]
     omega = mof.unit_volume
-    conversion_matrix = np.array([[1 / a, -np.cos(gamma) / (a * np.sin(gamma)),
-                                   b * c * (np.cos(alpha) * np.cos(gamma) - np.cos(beta)) / (omega * np.sin(gamma))],
-                                  [0, 1 / (b * np.sin(gamma)),
-                                   a * c * (np.cos(beta) * np.cos(gamma) - np.cos(alpha)) / (omega * np.sin(gamma))],
-                                  [0, 0, a * b * np.sin(gamma) / omega]])
+    conversion_matrix = np.array([[1 / a, -math.cos(gamma) / (a * math.sin(gamma)),
+                                   b * c * (math.cos(alpha) * math.cos(gamma) - math.cos(beta)) / (omega * math.sin(gamma))],
+                                  [0, 1 / (b * math.sin(gamma)),
+                                   a * c * (math.cos(beta) * math.cos(gamma) - math.cos(alpha)) / (omega * math.sin(gamma))],
+                                  [0, 0, a * b * math.sin(gamma) / omega]])
     return np.matmul(conversion_matrix, np.array([atom_x, atom_y, atom_z]))
